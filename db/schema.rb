80234cdf138f358db260a44581bf997dc20381c0
boris <boris.stoy@gmail.com>
2019-10-14 18:07:23 -0400

    Update gems, DB and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_09_07_170305) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.text "description"
