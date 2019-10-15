3908ef535514a7c0302c5cfbda37b234ebda1f46
boris <boris.stoy@gmail.com>
2019-10-15 09:46:38 -0400

    Make user auth public method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::API
     attr_reader :current_user
 
-    private
-
     def authenticate_request
         @current_user = AuthorizeApiRequest.call(request.headers).result
         render json: { error: 'Not Authorized' }, status: 401 unless @current_user
