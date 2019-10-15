9f13004429fc4c43e5dc52eb4b7712c345dd6dcb
boris <boris.stoy@gmail.com>
2019-10-15 12:13:46 -0400

    Remove secret key

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,9 +2,6 @@ class JsonWebToken
     class << self
         def encode(payload, exp = 24.hours.from_now)
             payload[:exp] = exp.to_i
-            puts "=================================="
-            puts Rails.application.credentials.secret_key_base
-            puts "=================================="
             JWT.encode(payload, Rails.application.credentials.secret_key_base)
         end
    
