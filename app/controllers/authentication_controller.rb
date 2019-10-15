94b013b4d706432466f6ce7f26ef4f030ccec59f
boris <boris.stoy@gmail.com>
2019-10-15 11:38:51 -0400

    Rollback

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -2,7 +2,7 @@ class AuthenticationController < ApplicationController
     skip_before_action :authenticate_request
    
     def authenticate
-      command = AuthenticateUser.call(params[:data][:email], params[:data][:password])
+      command = AuthenticateUser.call(params[:email], params[:password])
    
       if command.success?
         render json: { auth_token: command.result }
