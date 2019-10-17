05d57447572e614d8e143d9f53c70c50111e4588
boris <boris.stoy@gmail.com>
2019-10-17 17:25:43 -0400

    Add seeds

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'puma', '~> 3.11'
 gem 'bcrypt', '~> 3.1.7'
 gem 'jwt'
 gem 'simple_command'
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
 
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 # gem 'jbuilder', '~> 2.5'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'    
+
+puts "Seeding users......."
+User.destroy_all
+ActiveRecord::Base.connection.reset_pk_sequence!('users')
+
+User.create!(
+    email: 'test@test.com',
+    password: '123456'
+)
+
+puts "Seeding places........"
+Place.destroy_all
+ActiveRecord::Base.connection.reset_pk_sequence!('places')
+
+Place.create!(
+    name: Faker::Company.catch_phrase,
+    description: Faker::Lorem.sentence(word_count: 25),
+    address: Faker::Address.full_address
+)
+
+puts "Done!"
+
