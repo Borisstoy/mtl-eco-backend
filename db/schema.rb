a50bcbec05b5f2e0c1ff87f58111de2c9a694ed5
boris <boris.stoy@gmail.com>
2019-10-17 16:35:38 -0400

    Create Places model, delete items

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_07_170305) do
+ActiveRecord::Schema.define(version: 2019_10_17_203303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "items", force: :cascade do |t|
+  create_table "places", force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
