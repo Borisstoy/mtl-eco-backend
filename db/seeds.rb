152bf80333b97643c8e5344762148d06c78a580e
boris <boris.stoy@gmail.com>
2019-10-17 17:30:24 -0400

    Improve seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,11 +13,13 @@ puts "Seeding places........"
 Place.destroy_all
 ActiveRecord::Base.connection.reset_pk_sequence!('places')
 
-Place.create!(
-    name: Faker::Company.catch_phrase,
-    description: Faker::Lorem.sentence(word_count: 25),
-    address: Faker::Address.full_address
-)
+6.times do |i|
+    Place.create!(
+        name: Faker::Company.catch_phrase,
+        description: Faker::Lorem.sentence(word_count: 25),
+        address: Faker::Address.full_address
+    )
+  end
 
 puts "Done!"
 
