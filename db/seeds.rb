c9048ee3f389543b47387832bf462daffd2dddc7
Borisstoy <boris.stoy@gmail.com>
2020-04-03 18:59:44 -0400

    Use Montreal based addresses for seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,11 +13,20 @@ puts "Seeding places........"
 Place.destroy_all
 ActiveRecord::Base.connection.reset_pk_sequence!('places')
 
-6.times do |i|
+addresses = [
+    '6817 43 Av Montreal QC H1T 2R9',
+    '7503 Rue St Denis Montreal QC H2R 2E7',
+    '251 Av Percival Montreal Ouest QC H4X 1T8',
+    '7766 George Street Lasalle QC H8P 1E1',
+    '11727 Rue Notre Dame E Montreal QC H1B 2X8',
+    '5745 17 Av Montreal QC H1X 2R7'
+]
+
+addresses.each do |address| 
     Place.create!(
         name: Faker::Company.catch_phrase,
         description: Faker::Lorem.sentence(word_count: 25),
-        address: Faker::Address.full_address
+        address: address
     )
   end
 
